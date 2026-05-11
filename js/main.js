cd21abaf1d13a14503311e8bc4d91804dfc5cd04
VictorFrancodaCosta <victorfrancotst@gmail.com>
2026-05-11 06:24:07 -0300

    Add cluster_jogador.webp to image assets

--- js/main.js
+++ js/main.js
@@ -68,6 +68,7 @@ const ASSETS_TO_LOAD = {
         'assets/img/carta_descansar_mago.webp',
         'assets/img/carta_desarmar_mago.webp',
         'assets/img/carta_treinar_mago.webp'
+        'assets/img/cluster_jogador.webp'
     ],
     audio: [
         { id: 'bgm-menu', src: 'assets/audio/musica_menu.wav', loop: true }, 
